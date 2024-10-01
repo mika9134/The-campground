c5e6d34def23d1f3808b6d6e2934454ee0642279
[Mikiyas] <[mikasol9134@gmail.com]>
2024-10-01 07:15:28 -0400

    connection 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/camping-app',{
     useNewUrlParser: true ,
-          useUnifiedTopology: true 
+         // useUnifiedTopology: true 
 
 });
 
