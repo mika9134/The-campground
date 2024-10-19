4ad2ae0dfa1abfc0b2f711af2c2c3b6fd6fe58aa
mikkel <73791163+mika9134@users.noreply.github.com>
2024-10-19 11:38:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/The-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/The-camp');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
